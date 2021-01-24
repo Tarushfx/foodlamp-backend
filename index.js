79188033c667df5f001f8ddcee4970073a4e4b39
Tarush Bajaj <54022264+Tarushfx@users.noreply.github.com>
2021-01-24 18:01:32 +0530

    temp

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ import cors from "cors";
 import feed from "./routes/feed.js";
 import login from "./routes/login.js";
 import register from "./routes/register.js";
+import like from "./routes/like.js";
 
 const app = express();
-mongoose.connect('mongodb://localhost/foodlamp', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost/foodlamp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 app.use(express.json());
 app.use(cors());
-app.use('/feed', feed);
+app.use("/feed", feed);
 app.use("/login", login);
 app.use("/register", register);
-
+app.use("/like", like);
 
 const PORT = 4000;
-app.listen(process.env.PORT || PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(process.env.PORT || PORT, () =>
+  console.log(`Listening on port ${PORT}`)
+);
--- routes/feed.js
+++ routes/feed.js
@@ -1,10 +1,18 @@
 import express from "express";
 import _ from "lodash";
+import mongoose from "mongoose";
+import user from "../models/user.js";
 const feed = express.Router();
-feed.post("/", (req, res) => {
-    res.json(_.concat({ "hello": "hello" },
-        req.body)
-    );
-
+const User = mongoose.model("users", user.userSchema);
+feed.get("/", async (req, res) => {
+  try {
+    const email = req.body.email;
+    let foundUser = await User.findOne({ email });
+    if (foundUser) {
+      res.send(foundUser);
+    } else res.status(400);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 export default feed;
