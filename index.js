ddf36a5c7a3a462b55684cfb0f0cc1a4158a1330
Tarush Bajaj <54022264+Tarushfx@users.noreply.github.com>
2021-06-26 11:47:40 +0530

    deployment phase 1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import helmet from 'helmet';
+import compression from 'compression';
 import feed from "./routes/feed.js";
 import login from "./routes/login.js";
 import register from "./routes/register.js";
@@ -15,6 +17,8 @@ mongoose.connect("mongodb://localhost/foodlamp", {
 });
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
+app.use(compression());
 app.use("/feed", feed);
 app.use("/login", login);
 app.use("/register", register);
